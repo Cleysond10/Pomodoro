9dae8b9671c3d0ea7fcc27462621d2bd7fb6c730
Cleyson Henrique Silva Duarte <cleysonduarte99@gmail.com>
2021-05-13 18:06:40 -0300

    Create jest tests

---
+++ frontend/src/App.test.jsx
@@ -0,0 +1,12 @@
+// import { render } from "react-dom";
+// import Button from "./components/Button";
+
+// describe("test App home page", () => {
+//   it("Verify button content", () => {
+//     const { getByTestId } = render(<Button />);
+
+//     const button = getByTestId("button");
+
+//     expect(button).toHaveTextContent("Iniciar Pomodoro");
+//   });
+// });
---
+++ frontend/src/components/Button.test.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Button from "./Button";
+
+describe("test App home page", () => {
+  it("Verify button content", () => {
+    render(<Button />);
+
+    const button = screen.getByTestId("button");
+
+    expect(button).not.toBeDisabled();
+  });
+});
