33b6b990a0934adfb614ebf9839a79365cd69c20
Cleyson Henrique Silva Duarte <cleysonduarte99@gmail.com>
2021-05-13 18:05:53 -0300

    Create button component with material ui

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+
+export default function Btn() {
+  return (
+    <Button color="secondary" data-testid="button">
+      Iniciar Pomodoro
+    </Button>
+  );
+}
