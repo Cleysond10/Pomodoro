e6b18b33d15c659a6e08fc0bdf6b90817befb8a8
Cleyson Henrique Silva Duarte <cleysonduarte99@gmail.com>
2021-05-16 03:01:42 -0300

    Code refactor

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 
-export default function Btn() {
-  return (
-    <Button color="secondary" data-testid="button">
-      Iniciar Pomodoro
-    </Button>
-  );
+export default function index({ children, ...otherProps }) {
+  return <Button color="secondary">{children}</Button>;
 }
