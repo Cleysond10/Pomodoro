e0b4dd64c96739f447e9cc307cc81409c1b27828
Cleyson Henrique Silva Duarte <cleysonduarte99@gmail.com>
2021-05-19 19:19:30 -0300

    Create timer component

---
+++ frontend/src/components/Timer/index.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+// import Button from "./components/Button/Button";
+
+export default function Timer() {
+  const [time, setTime] = useState(25);
+
+  const timer = () => {
+    if (time > 0) {
+      timeout();
+    }
+    console.log(time);
+  };
+
+  const timeout = () => {
+    setTimeout(() => {
+      setTime(time - 1);
+    }, 1000);
+  };
+  return (
+    <div>
+      <p>WELCOME TO YOUR POMODORO APP!</p>
+      <button type="submit" onClick={timer}>
+        {" "}
+        INICIAR POMODORO{" "}
+      </button>
+      <h1> {`Tempo Restante: ${time}`} </h1>
+    </div>
+  );
+}
