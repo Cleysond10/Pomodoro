555563580d33286ea3fb86325ea2041d8c0f63c3
Cleyson Henrique Silva Duarte <cleysonduarte99@gmail.com>
2021-05-19 19:20:00 -0300

    Minor fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import Box from "@material-ui/core/Box";
+import Timer from "./components/Timer";
 import Camera from "./components/Camera/index";
-// import Button from "./components/Button/Button";
 
 function App() {
-  const [time, setTime] = useState(25);
-
-  const timer = () => {
-    if (time > 0) {
-      timeout();
-    }
-    console.log(time);
-  };
-
-  const timeout = () => {
-    setTimeout(() => {
-      setTime(time - 1);
-    }, 1000);
-  };
-
   return (
     <div>
       <Box border={1} p={20}>
-        <p>WELCOME TO YOUR POMODORO APP!</p>
-        <button type="submit" onClick={timer}>
-          {" "}
-          INICIAR POMODORO{" "}
-        </button>
-        <h1> {`Tempo Restante: ${time}`} </h1>
+        <Timer></Timer>
       </Box>
       <Box border={1} p={20}>
         <Camera></Camera>
