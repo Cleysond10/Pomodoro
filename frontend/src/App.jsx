0f82affad34d613b2b2597ca92f2702a259cc141
Cleyson Henrique Silva Duarte <cleysonduarte99@gmail.com>
2021-06-12 23:06:36 -0300

    Remove camera from app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import Box from "@material-ui/core/Box";
 import Timer from "./components/Timer";
-import Camera from "./components/Camera/index";
 
 function App() {
   return (
     <div>
       <Box border={1} p={20}>
         <Timer></Timer>
-      </Box>
-      <Box border={1} p={20}>
-        <Camera></Camera>
       </Box>
     </div>
   );
