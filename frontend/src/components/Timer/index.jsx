8f6ad1790c3bc40ffac5833e83d902829fd8a67f
Cleyson Henrique Silva Duarte <cleysonduarte99@gmail.com>
2022-04-15 21:38:16 -0300

    Fix count down clock

--- frontend/src/components/Timer/index.jsx
+++ frontend/src/components/Timer/index.jsx
@@ -3,22 +3,36 @@ import React, { useState } from "react";
 export default function Timer() {
   const [time, setTime] = useState(25);
 
-  const timer = () => {
-    if (time > 0) {
-      timeout();
-    }
-    console.log(time);
-  };
-
-  const timeout = () => {
+  const handleTime = (count) => {
     setTimeout(() => {
-      setTime(time - 1);
+      if(count > 0){
+        setTime(count - 1);
+        return handleTime(count - 1);
+      }
     }, 1000);
-  };
+  }
+
+  // useEffect(() => {
+  //   handleTime();
+  // }, [time]);
+
+  // const handleTime = () => {
+  //   if (time > 0) {
+  //     onTimeout();
+  //   }
+  //   console.log(time);
+  // };
+
+  // const onTimeout = () => {
+  //   setTimeout(() => {
+  //     setTime(time - 1);
+  //   }, 1000);
+  // };
+  
   return (
     <div>
       <p>WELCOME TO YOUR POMODORO APP!</p>
-      <button type="submit" data-testid="button_test" onClick={timer}>
+      <button type="submit" data-testid="button_test" onClick={() => handleTime(time)}>
         {" "}
         INICIAR POMODORO{" "}
       </button>
